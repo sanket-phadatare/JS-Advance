4652e6c70cc82f0d84771b38a940ccbed9447067
sanket-phadatare <sanketphadatare1718@gmail.com>
2026-04-13 11:51:56 +0530

    First change by developer sanket

---
+++ Scripts/Home.js
@@ -0,0 +1,15 @@
+function Changetext(){
+    document.getElementById("heading").innerHTML = "Paragraph changed.";
+}
+
+function Showoutput(){
+    //alert("Button clicked");
+    //confirm("Are you sure you want to click the button?");
+    //document.write("Button clicked");
+    //document.getElementById("output").innerHTML = "Button clicked";
+
+    console.log("Button clicked");
+    console.warn("This is a warning message");
+    console.error("This is an error message");
+}
+
